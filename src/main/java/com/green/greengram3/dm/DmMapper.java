1e0808589a80d7a63f917d8d65da54dc79643611
aeyoung <aeyoung0325@naver.com>
2023-12-15 17:00:33 +0900

    23. 12. 15.

--- src/main/java/com/green/greengram3/dm/DmMapper.java
+++ src/main/java/com/green/greengram3/dm/DmMapper.java
@@ -21,7 +21,6 @@ public interface DmMapper {
 
 
     //----------------------------------- t_dm_msg
-    int updDmLstMsg (DmMsgInsDto dto);
     List<DmMsgSelVo> selDmMsgAll (DmMsgSelDto dto); // dm 전체 화면 출력
     int insDmMsg(DmMsgInsDto dto); // dm 보내기
     int delDmMsg(DmMsgDelDto dto); // dm 삭제
--- src/main/java/com/green/greengram3/dm/DmService.java
+++ src/main/java/com/green/greengram3/dm/DmService.java
@@ -79,29 +79,68 @@ public class DmService {
     }
 
     public DmSelVo postDm(DmInsDto dto){
-        mapper.insDm(dto); // t_dm 테이블에 null값을 넣어서 idm pk 값 생성
+        Integer isExistDm = mapper.selDmUserCheck(dto);
+        log.info("dto: {}", dto);
+        if (isExistDm != null) {
+            return null;
+        }
 
-        mapper.insDmUser(DmUserInsDto.builder()
-                .idm(dto.getIdm())
+        mapper.insDm(dto); // 빈 방 만들기 idm 값 생성
+        mapper.insDmUser(DmUserInsDto.builder() // 로그인한 유저 해당 방에 참여 시키는 과정
+                .idm(dto.getIdm()) // 빈 방 생성시 만들어진 idm값 가져옴
                 .iuser(dto.getLoginedIuser())
                 .build());
-        mapper.insDmUser(DmUserInsDto.builder()
-                .idm(dto.getIdm())
-                .iuser(dto.getOtherPersonIuser())
+
+        mapper.insDmUser(DmUserInsDto.builder() // 다른 유저 해당 방에 참여
+                .idm(dto.getIdm()) // 빈 방 생성시 만들어진 idm값 가져옴
+                .iuser(dto.getOtherPersonIuser()) //postman에서 입력받은 상대 iuser값 가져옴
                 .build());
-        // 생성된 대화방(idm)에 대화자(iuser) 두 명 넣기
+        // 여기까지 새로 생성된 채팅방에 유저 집어넣기완료
 
-        UserSelDto uDto= new UserSelDto(); // iuser, uid 값을 사용하기 위해 객체 생성
-        uDto.setIuser(dto.getOtherPersonIuser());
-        UserEntity entity= userMapper.selUser(uDto); // dm방에서 상대의 프로필 불러오기
+        UserSelDto usDto = new UserSelDto(); // UserMapper.xml에 있는 selUser를 실행하기 위해 객체생성
+        usDto.setIuser(dto.getOtherPersonIuser()); // 새로운객체 usDto에 dto에서 입력받은 상대 iuser값 가져옴
 
-        // lastMsg, lastMsgAt 값 null
-        return DmSelVo.builder().idm(dto.getIdm())
-                .otherPersonIuser(entity.getIuser())
-                .otherPersonNm(entity.getNm())
-                .otherPersonPic(entity.getPic())
+        UserEntity entity = userMapper.selUser(usDto); // 다른 유저의 정보 얻어오기
+        return DmSelVo.builder() // 빌더로 값을 불러온다.
+                .idm(dto.getIdm()) // 위에서 생성된 방번호 가져오기
+                .otherPersonIuser(entity.getIuser()) // 다른 유저의 iuser 가져오기
+                .otherPersonNm(entity.getNm()) // 다른 유저의 nm 가져오기
+                .otherPersonPic(entity.getPic()) // 다른 유저의 사진 가져오기
                 .build();
     }
+
+//    public DmSelVo postDm(DmInsDto dto){
+//
+//        Integer isExistDm = mapper.selDmUserCheck(dto);
+//        log.info("dto: {}", dto);
+//        if (isExistDm != null) {
+//            return null;
+//        }
+//
+//        int dmAffectedRows = mapper.insDm(dto);  // t_dm 테이블에 null값을 넣어서 idm pk 값 생성
+//
+//
+//        mapper.insDmUser(DmUserInsDto.builder()
+//                .idm(dto.getIdm())
+//                .iuser(dto.getLoginedIuser())
+//                .build());
+//        mapper.insDmUser(DmUserInsDto.builder()
+//                .idm(dto.getIdm())
+//                .iuser(dto.getOtherPersonIuser())
+//                .build());
+//        // 생성된 대화방(idm)에 대화자(iuser) 두 명 넣기
+//
+//        UserSelDto uDto= new UserSelDto(); // iuser, uid 값을 사용하기 위해 객체 생성
+//        uDto.setIuser(dto.getOtherPersonIuser());
+//        UserEntity entity= userMapper.selUser(uDto); // dm방에서 상대의 프로필 불러오기
+//
+//        // lastMsg, lastMsgAt 값 null
+//        return DmSelVo.builder().idm(dto.getIdm())
+//                .otherPersonIuser(entity.getIuser())
+//                .otherPersonNm(entity.getNm())
+//                .otherPersonPic(entity.getPic())
+//                .build();
+//    }
 
     public List<DmMsgSelVo> getMsgAll(DmMsgSelDto dto) {
         log.info("dto:{}", dto);
